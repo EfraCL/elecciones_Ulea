7c8f52a11a54ee1222618bd883bc92fb246c9940
Efraín <efrainclulea@hotmail.com>
2023-05-30 19:27:53 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,27 @@
+# Datos elecciones en Ulea
+
+En este repositorio colgaré los datos relativos al número de personas por hora que acudieron a votaren elecciones celebradas en Ulea, a partir del año 2023 (inclusive). Estos datos incluirán los siguientes ámbitos administrativos: **comunitario**, **estatal**, **autonómico** y **municipal**.
+
+## Año 2023
+
+### Votaciones del día 28 de Mayo (columna 'Mun_Aut_280523')
+
+Se celebraron las siguientes elecciones:
+
+[] Comunitaria
+[] Estatal
+[x] Autonómica
+[x] Municipal
+
+**Importante 1**: Los datos referentes a los asistentes durante los períodos 09:00 - 10:00 y 10:00 - 11:00 fueron estimados a partir del total de datos recabados desde las 09:00 a las 11:00. Concretamente se estimó que el aproximadamente el 30 % del total (~35 personas) acudió de 09:00 - 10:00, mientras que el resto (70 % u 82 personas) acudió de 10:00 a 11:00.
+
+**Importante 2**: Los datos del período 19:00:00 a 20:00:00 incluyen también los votos del os interventores (4) y los miembros de la mesa electoral (3).
+
+**Importante 3**: Los datos no incluyen el número de votos por correo.
+
+### Votaciones del día ... de Julio (columna 'Est_23')
+
+[] Comunitaria
+[] Estatal
+[] Autonómica
+[] Municipal
---
+++ analisis_Mun_Aut_280523.R
@@ -0,0 +1,71 @@
+# Carga de paquetes y datos ----
+library(ggplot2)
+library(lubridate)
+
+colClas <- c("character", "character", "integer")
+x <- read.csv("voto_horario_Ulea.csv", header = T, sep = ";",
+              colClasses = colClas)
+
+# Formateo de datos ----
+
+x$Hora_ini <- as.POSIXct(x$Hora_ini, format = "%H:%M:%S")
+x$Hora_fin <- as.POSIXct(x$Hora_fin, format = "%H:%M:%S")
+x$temp <- x$Hora_ini+(x$Hora_fin-x$Hora_ini)/2
+x$temp <- seq(as.POSIXct("09:30:00", format = "%H:%M:%S"),
+              as.POSIXct("19:30:00", format = "%H:%M:%S"),
+              "1 hour")
+
+# Gráficos ----
+
+## Histograma por horas ----
+ggplot(x, aes(x = temp, y = N_Mun_Aut_280523))+
+  geom_col(fill = "#6ce76a")+
+  geom_text(aes(label = N_Mun_Aut_280523), 
+            position = position_dodge(width = 0.9), 
+            vjust = -0.25)+
+  scale_x_datetime(breaks = "1 hour", date_labels = "%H:%M")+
+  scale_y_continuous(breaks = seq(0, 120, 10))+
+  labs(x = "Hora", y = "Número de votantes")+
+  theme_classic()
+
+ggsave("Histograma_por_horas.png")
+  
+## Histograma acumulado por horas ----
+
+ggplot(x, aes(x = temp, y = cumsum(N_Mun_Aut_280523)))+
+  geom_col(fill = "#6ce76a")+
+  geom_text(aes(label = cumsum(N_Mun_Aut_280523)), 
+            position = position_dodge(width = 0.9), 
+            vjust = -0.25)+
+  scale_x_datetime(breaks = "1 hour", date_labels = "%H:%M")+
+  scale_y_continuous(breaks = seq(0, 700, 50))+
+  labs(x = "Hora", y = "Número de votantes")+
+  theme_classic()
+ggsave("Histograma_acumulado_por_horas.png")
+
+# Distribucion de Poisson ----
+
+lambda <- mean(x$N_Mun_Aut_280523)
+
+## Gráfico de función de masa de Poisson ----
+x <- seq(0, 110, 1)
+prob <- dpois(x, lambda = lambda)
+temp <- data.frame(N_votantes = x, Probabilidad = prob)
+
+ggplot(temp, aes(x = N_votantes, y = Probabilidad))+
+  geom_line()
+
+## Gráfico de función acumulada de Poisson ----
+
+x <- seq(0, 110, 1)
+prob <- ppois(x, lambda = lambda)
+temp <- data.frame(N_votantes = x, Probabilidad = prob)
+
+ggplot(temp, aes(x = N_votantes, y = Probabilidad*100))+
+  geom_line()+
+  labs(x = "Número de votantes en 1 hora", y = "Probabilidad (%)")+
+  scale_x_continuous(breaks = seq(0, 110, 5))+
+  theme_classic()
+
+ppois(100, lambda = lambda) - ppois(35, lambda = lambda) # Prob de que en una hora vengan entre 100 y 35 votantes
+
